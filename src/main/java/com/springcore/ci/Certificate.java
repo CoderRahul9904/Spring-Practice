d5e2e48bc4066d51469bb605372441b44bc43bfb
Rahul <mouryarahul543@gmail.com>
2025-10-02 00:33:35 +0530

    added certificate class

--- src/main/java/com/springcore/ci/Certificate.java
+++ src/main/java/com/springcore/ci/Certificate.java
@@ -1,4 +1,24 @@
 package com.springcore.ci;
 
+import java.util.List;
+
 public class Certificate {
+    private String certificate;
+    private List<String> topics;
+
+    public Certificate(String certificate, List<String> topics) {
+        this.certificate = certificate;
+        this.topics = topics;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+    public List<String> getTopics() {
+        return topics;
+    }
+    @Override
+    public String toString() {
+        return "Certificate [certificate=" + certificate + ", topics=" + topics + "]";
+    }
 }
