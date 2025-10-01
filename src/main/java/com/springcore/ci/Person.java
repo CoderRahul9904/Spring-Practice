0c7e710eb6e51dca7579d790b18910016714bb05
Rahul <mouryarahul543@gmail.com>
2025-10-02 00:33:54 +0530

    added person class

--- src/main/java/com/springcore/ci/Person.java
+++ src/main/java/com/springcore/ci/Person.java
@@ -3,13 +3,15 @@ package com.springcore.ci;
 public class Person {
     private String name;
     private int age;
+    private Certificate cert;
 
-    public Person(String name, int age) {
+    public Person(String name, int age, Certificate cert) {
         this.name = name;
         this.age = age;
+        this.cert = cert;
     }
     @Override
     public String toString() {
-        return "Person [name=" + name + ", age=" + age + "]";
+        return "Person [name=" + name + ", age=" + Integer.toString(age) + ", cert=" + cert.getCertificate()+ ", topics=" + cert.getTopics()  + "]";
     }
 }
