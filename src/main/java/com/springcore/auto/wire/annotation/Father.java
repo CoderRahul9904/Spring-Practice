d9a04602b13e6f962f44b596ea0c2a961beaa32b
Rahul <mouryarahul543@gmail.com>
2025-10-03 23:48:49 +0530

    learn about qualifier annotation

--- src/main/java/com/springcore/auto/wire/annotation/Father.java
+++ src/main/java/com/springcore/auto/wire/annotation/Father.java
@@ -1,11 +1,13 @@
 package com.springcore.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 
 public class Father {
     private String fatherName;
-
+    @Autowired
+    @Qualifier("mother123")
     private Mother mother;
 
     public String getFatherName() {
@@ -17,7 +19,7 @@ public class Father {
     public String getMotherName() {
         return mother.getFirstName();
     }
-    @Autowired
+
     public Father(Mother mother) {
         this.mother = mother;
     }
