949fe62869126ef17d5972f54b6e9dc9dba60cfc
Rahul <mouryarahul543@gmail.com>
2025-10-05 15:29:55 +0530

    add student example to understand stereotype

---
+++ src/main/java/com/springcore/steretype/App.java
@@ -0,0 +1,15 @@
+package com.springcore.steretype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("stereotypeconfig.xml");
+        Student student=context.getBean("obj",Student.class);
+        System.out.println(student);
+    }
+
+
+
+}
---
+++ src/main/java/com/springcore/steretype/Student.java
@@ -0,0 +1,41 @@
+package com.springcore.steretype;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("obj")
+public class Student {
+    @Value("Rahul")
+    private String studentName;
+    @Value("h162dc")
+    private String studentID;
+    @Value("Male")
+    private String studentGender;
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+    public void setStudentGender(String studentGender) {
+        this.studentGender = studentGender;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public String getStudentID() {
+        return studentID;
+    }
+    public String getStudentGender() {
+        return studentGender;
+    }
+    public Student(){
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Student [studentName=" + studentName + ", studentID=" + studentID + ", studentGender=" + studentGender + "]";
+    }
+}
