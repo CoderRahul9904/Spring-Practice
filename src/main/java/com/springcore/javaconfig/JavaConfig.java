a12719965809d2f6cfa24cebc71e34d46bc90b3d
Rahul <mouryarahul543@gmail.com>
2025-10-06 21:22:29 +0530

    use of @Bean annotation

--- src/main/java/com/springcore/javaconfig/JavaConfig.java
+++ src/main/java/com/springcore/javaconfig/JavaConfig.java
@@ -1,9 +1,23 @@
 package com.springcore.javaconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.springcore.javaconfig")
 public class JavaConfig {
+    @Bean({"mother","info"})
+    public Mother getMotherInfo(Father getFatherInfo, Child getChildInfo) {
+        return new Mother(getFatherInfo, getChildInfo);
+    }
+
+    @Bean
+    public Father getFatherInfo(){
+        return new Father();
+    }
+
+    @Bean
+    public Child getChildInfo(){
+        return new Child();
+    }
 }
