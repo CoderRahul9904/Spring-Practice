44d993459e339207af32586938eb9bc34b70863b
Rahul <mouryarahul543@gmail.com>
2025-10-24 22:34:00 +0530

    added sql query

--- src/main/java/com/springcore/jdbc/basic/App.java
+++ src/main/java/com/springcore/jdbc/basic/App.java
@@ -1,7 +1,17 @@
 package com.springcore.jdbc.basic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class App {
     public static void main(String[] args) {
-        
+        ApplicationContext context = new ClassPathXmlApplicationContext("jdbcconfig.xml");
+        JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
+
+        //sql query
+        String sql = "insert into students(sid,name,surname,address) values(?,?,?,?)";
+        int result=jdbcTemplate.update(sql,21,"Rahul","Mourya","Near Jai Hind High School Pimpri - 17");
+        System.out.println(result);
     }
 }
