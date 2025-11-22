68b950671ee8022346cdf8a1f076c6098fd50884
Rahul <mouryarahul543@gmail.com>
2025-11-22 20:00:24 +0530

    update the Dao

--- src/main/java/com/springcore/orm/App.java
+++ src/main/java/com/springcore/orm/App.java
@@ -1,4 +1,20 @@
 package com.springcore.orm;
 
+import com.springcore.orm.dao.StudentDao;
+import com.springcore.orm.dao.StudentDaoImpl;
+import com.springcore.orm.entities.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("springHibernateConfig.xml");
+        StudentDaoImpl studentDao = (StudentDaoImpl) context.getBean("StudentDao");
+        Student student = new Student();
+        student.setSname("Ananya");
+        student.setSage("Sharma");
+        student.setAddress("Pune");
+
+        studentDao.insert(student);
+    }
 }
