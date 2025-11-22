1820d1c60e5780f9338fdaa859e9564cfe3ded42
Rahul <mouryarahul543@gmail.com>
2025-11-22 20:00:52 +0530

    added

--- src/main/java/com/springcore/orm/dao/StudentDaoImpl.java
+++ src/main/java/com/springcore/orm/dao/StudentDaoImpl.java
@@ -1,5 +1,10 @@
 package com.springcore.orm.dao;
 
+import com.springcore.orm.entities.Student;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
 public interface StudentDaoImpl {
+    public int insert(Student student);
+    public HibernateTemplate getHibernateTemplate();
 
 }
